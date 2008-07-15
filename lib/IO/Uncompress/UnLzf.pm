283c91fdcdedfa8d0c1c40f40812e4cc692ad6c1
pmqs <pmqs@cpan.org>
2008-07-15 22:41:49 +0100

    import IO-Compress-Lzf 2.012
      2.012 15 July 2008
    
          * No Changes
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.012
    git-cpan-authorid: pmqs

--- lib/IO/Uncompress/UnLzf.pm
+++ lib/IO/Uncompress/UnLzf.pm
@@ -4,16 +4,16 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.011 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.012 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.011 ;
-use IO::Uncompress::Adapter::Lzf  2.011 ;
+use IO::Uncompress::Base  2.012 ;
+use IO::Uncompress::Adapter::Lzf  2.012 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzfError);
 
-$VERSION = '2.011';
+$VERSION = '2.012';
 $UnLzfError = '';
 
 @ISA    = qw( Exporter IO::Uncompress::Base );
