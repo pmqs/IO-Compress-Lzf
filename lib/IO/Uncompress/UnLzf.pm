c92ae2ac9b4ff8b5668b348df20f11fd86b3a1e8
pmqs <pmqs@cpan.org>
2008-09-03 20:49:16 +0100

    import IO-Compress-Lzf 2.015
      2.015 3 September 2008
    
          * Makefile.PL
            Backout changes made in 2.014
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.015
    git-cpan-authorid: pmqs

--- lib/IO/Uncompress/UnLzf.pm
+++ lib/IO/Uncompress/UnLzf.pm
@@ -4,16 +4,16 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.014 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.015 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.014 ;
-use IO::Uncompress::Adapter::Lzf  2.014 ;
+use IO::Uncompress::Base  2.015 ;
+use IO::Uncompress::Adapter::Lzf  2.015 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzfError);
 
-$VERSION = '2.014';
+$VERSION = '2.015';
 $UnLzfError = '';
 
 @ISA    = qw( Exporter IO::Uncompress::Base );
