849e29a6a5c7000c4ab67c54cc557f7a4c676b97
pmqs <pmqs@cpan.org>
2011-12-04 19:21:52 +0000

    import IO-Compress-Lzf 2.045
      2.045 3 December 2011
    
          * Moved FAQ.pod to IO::Compress
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.045
    git-cpan-authorid: pmqs

--- lib/IO/Compress/Lzf.pm
+++ lib/IO/Compress/Lzf.pm
@@ -5,15 +5,15 @@ use warnings;
 require Exporter ;
 use bytes;
 
-use IO::Compress::Base 2.044 ;
+use IO::Compress::Base 2.045 ;
 
-use IO::Compress::Base::Common  2.044 qw(createSelfTiedObject);
-use IO::Compress::Adapter::Lzf  2.044 ;
+use IO::Compress::Base::Common  2.045 qw(createSelfTiedObject);
+use IO::Compress::Adapter::Lzf  2.045 ;
 #use Compress::Lzf ;
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $LzfError);
 
-$VERSION = '2.044';
+$VERSION = '2.045';
 $LzfError = '';
 
 @ISA    = qw(Exporter IO::Compress::Base);
@@ -98,7 +98,7 @@ sub getExtraParams
 {
     my $self = shift ;
 
-    use IO::Compress::Base::Common  2.044 qw(:Parse);
+    use IO::Compress::Base::Common  2.045 qw(:Parse);
     
     return (
             'BlockSize' => [0, 1, Parse_unsigned,  (64* 1024) - 1],
--- lib/IO/Uncompress/UnLzf.pm
+++ lib/IO/Uncompress/UnLzf.pm
@@ -4,16 +4,16 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.044 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.045 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.044 ;
-use IO::Uncompress::Adapter::Lzf  2.044 ;
+use IO::Uncompress::Base  2.045 ;
+use IO::Uncompress::Adapter::Lzf  2.045 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzfError);
 
-$VERSION = '2.044';
+$VERSION = '2.045';
 $UnLzfError = '';
 
 @ISA    = qw( Exporter IO::Uncompress::Base );
