1065d6988148a73fcc0a2965387ec4838646cddd
pmqs <pmqs@cpan.org>
2012-01-28 23:43:29 +0000

    import IO-Compress-Lzf 2.047
      2.047 28 January 2012
    
          * Set minimum Perl version to 5.6
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.047
    git-cpan-authorid: pmqs

--- lib/IO/Uncompress/UnLzf.pm
+++ lib/IO/Uncompress/UnLzf.pm
@@ -4,16 +4,16 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.045 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.047 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.045 ;
-use IO::Uncompress::Adapter::Lzf  2.045 ;
+use IO::Uncompress::Base  2.047 ;
+use IO::Uncompress::Adapter::Lzf  2.047 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzfError);
 
-$VERSION = '2.045';
+$VERSION = '2.047';
 $UnLzfError = '';
 
 @ISA    = qw( Exporter IO::Uncompress::Base );
@@ -954,7 +954,7 @@ See the Changes file.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2005-2011 Paul Marquess. All rights reserved.
+Copyright (c) 2005-2012 Paul Marquess. All rights reserved.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
