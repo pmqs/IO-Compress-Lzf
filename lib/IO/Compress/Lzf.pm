688899c37cfe84f61b1c6a31ea9675ca4b11bc12
pmqs <pmqs@cpan.org>
2012-01-29 17:00:52 +0000

    import IO-Compress-Lzf 2.048
      2.048 29 January 2012
    
          * No Changes
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.048
    git-cpan-authorid: pmqs

--- lib/IO/Compress/Lzf.pm
+++ lib/IO/Compress/Lzf.pm
@@ -5,15 +5,15 @@ use warnings;
 require Exporter ;
 use bytes;
 
-use IO::Compress::Base 2.047 ;
+use IO::Compress::Base 2.048 ;
 
-use IO::Compress::Base::Common  2.047 qw(createSelfTiedObject);
-use IO::Compress::Adapter::Lzf  2.047 ;
+use IO::Compress::Base::Common  2.048 qw(createSelfTiedObject);
+use IO::Compress::Adapter::Lzf  2.048 ;
 #use Compress::Lzf ;
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $LzfError);
 
-$VERSION = '2.047';
+$VERSION = '2.048';
 $LzfError = '';
 
 @ISA    = qw(Exporter IO::Compress::Base);
@@ -98,7 +98,7 @@ sub getExtraParams
 {
     my $self = shift ;
 
-    use IO::Compress::Base::Common  2.047 qw(:Parse);
+    use IO::Compress::Base::Common  2.048 qw(:Parse);
     
     return (
             'BlockSize' => [0, 1, Parse_unsigned,  (64* 1024) - 1],
