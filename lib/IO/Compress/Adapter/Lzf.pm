f4a832d806f3825d4413c71d7a662215563141b5
pmqs <pmqs@cpan.org>
2013-01-07 20:04:01 +0000

    import IO-Compress-Lzf 2.060
      2.060 7 January 2013
    
          * No Changes
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.060
    git-cpan-authorid: pmqs

--- lib/IO/Compress/Adapter/Lzf.pm
+++ lib/IO/Compress/Adapter/Lzf.pm
@@ -4,11 +4,11 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.059 qw(:Status);
+use IO::Compress::Base::Common  2.060 qw(:Status);
 use Compress::LZF ;
 
 our ($VERSION);
-$VERSION = '2.059';
+$VERSION = '2.060';
 
 use constant SIGNATURE => 'ZV';
 
