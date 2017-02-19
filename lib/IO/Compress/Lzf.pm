64164fe3fece2d5742ed9f81cab3cf058c44377e
pmqs <pmqs@cpan.org>
2017-02-19 22:12:05 +0000

    import IO-Compress-Lzf 2.074
      2.074 19 Feb 2017
    
          * Fix bad 2.073 release
    
    
    git-cpan-module:   IO-Compress-Lzf
    git-cpan-version:  2.074
    git-cpan-authorid: pmqs

--- lib/IO/Compress/Lzf.pm
+++ lib/IO/Compress/Lzf.pm
@@ -5,15 +5,15 @@ use warnings;
 require Exporter ;
 use bytes;
 
-use IO::Compress::Base 2.073 ;
+use IO::Compress::Base 2.074 ;
 
-use IO::Compress::Base::Common  2.073 qw(createSelfTiedObject);
-use IO::Compress::Adapter::Lzf  2.073 ;
+use IO::Compress::Base::Common  2.074 qw(createSelfTiedObject);
+use IO::Compress::Adapter::Lzf  2.074 ;
 #use Compress::Lzf ;
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $LzfError);
 
-$VERSION = '2.073';
+$VERSION = '2.074';
 $LzfError = '';
 
 @ISA    = qw( IO::Compress::Base Exporter );
--- lib/IO/Uncompress/UnLzf.pm
+++ lib/IO/Uncompress/UnLzf.pm
@@ -4,16 +4,16 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.073 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.074 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.073 ;
-use IO::Uncompress::Adapter::Lzf  2.073 ;
+use IO::Uncompress::Base  2.074 ;
+use IO::Uncompress::Adapter::Lzf  2.074 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzfError);
 
-$VERSION = '2.073';
+$VERSION = '2.074';
 $UnLzfError = '';
 
 @ISA    = qw( IO::Uncompress::Base Exporter );
