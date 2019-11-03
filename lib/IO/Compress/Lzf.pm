b8c9d1dae5e2e2362329a7943d6b7e3ce3606e8f
pmqs <pmqs@cpan.org>
2019-11-03 19:49:46 +0000

    update to version 2.089

--- lib/IO/Compress/Lzf.pm
+++ lib/IO/Compress/Lzf.pm
@@ -5,15 +5,15 @@ use warnings;
 require Exporter ;
 use bytes;
 
-use IO::Compress::Base 2.088 ;
+use IO::Compress::Base 2.089 ;
 
-use IO::Compress::Base::Common  2.088 qw(createSelfTiedObject);
-use IO::Compress::Adapter::Lzf  2.088 ;
+use IO::Compress::Base::Common  2.089 qw(createSelfTiedObject);
+use IO::Compress::Adapter::Lzf  2.089 ;
 #use Compress::Lzf ;
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $LzfError);
 
-$VERSION = '2.088';
+$VERSION = '2.089';
 $LzfError = '';
 
 @ISA    = qw( IO::Compress::Base Exporter );
