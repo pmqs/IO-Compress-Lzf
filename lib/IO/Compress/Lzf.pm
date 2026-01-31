70c3745fac91574b2ebb94b364b5d26802f7acd2
pmqs <pmqs@cpan.org>
2026-01-31 11:39:22 +0000

    Update version to 2.217

--- lib/IO/Compress/Lzf.pm
+++ lib/IO/Compress/Lzf.pm
@@ -5,15 +5,15 @@ use warnings;
 require Exporter ;
 use bytes;
 
-use IO::Compress::Base 2.214 ;
+use IO::Compress::Base 2.217 ;
 
-use IO::Compress::Base::Common  2.214 qw(createSelfTiedObject);
-use IO::Compress::Adapter::Lzf  2.214 ;
+use IO::Compress::Base::Common  2.217 qw(createSelfTiedObject);
+use IO::Compress::Adapter::Lzf  2.217 ;
 #use Compress::Lzf ;
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $LzfError);
 
-$VERSION = '2.214';
+$VERSION = '2.217';
 $LzfError = '';
 
 @ISA    = qw( IO::Compress::Base Exporter );
@@ -824,7 +824,7 @@ See the Changes file.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2005-2025 Paul Marquess. All rights reserved.
+Copyright (c) 2005-2026 Paul Marquess. All rights reserved.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
